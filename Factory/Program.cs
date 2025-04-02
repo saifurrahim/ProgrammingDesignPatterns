49c16e29f8ff98b1a14c3c81a11d3f5b0efedd4e
unknown <saifurrahimwp@gmail.com>
2025-04-02 22:32:53 +0700

    Make Factory example : add implementation of use in Program.cs

--- Factory/Program.cs
+++ Factory/Program.cs
@@ -1,6 +1,12 @@
 
 class Program {
     static void Main(string[] args) {
+        Delivery delivery = DeliveryFactory.createDelivery("courier");
+        delivery.calculateDeliveryCost();
+        delivery.processDelivery();
 
+        // Another example would be when the project require multiple type of database connection
+        // So you create Connection abstract class, then MySQLConnection and PostgreSQLConnection concrete class,
+        // and ConnectionFactory class with static function createConnection.
     }
 }
