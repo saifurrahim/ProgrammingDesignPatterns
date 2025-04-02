bde5c317281e824ae3de9110dbf2afc807f99bc2
unknown <saifurrahimwp@gmail.com>
2025-04-02 22:28:28 +0700

    Make Factory example : add delivery factory as factory class

---
+++ Factory/DeliveryFactory.cs
@@ -0,0 +1,14 @@
+public class DeliveryFactory {
+    public static Delivery createDelivery(string deliveryMethod)
+    {
+        switch(deliveryMethod)
+        {
+            case "courier" :
+                return new CourierDelivery("JNE");
+            case "post" :
+                return new PostDelivery("Mailman");
+            default:
+                throw new ArgumentException("Delivery method not supported");
+        }
+    }
+}
