6c38a278344bcbc7cde2b9de2cfb61db866f60bb
unknown <saifurrahimwp@gmail.com>
2025-04-02 22:19:55 +0700

    Make Factory example : add courier delivery as concrete class

---
+++ Factory/CourierDelivery.cs
@@ -0,0 +1,22 @@
+public class CourierDelivery : Delivery {
+    private string courierName;
+
+    public CourierDelivery(string courierName)
+    {
+        this.courierName = courierName;
+    }
+
+    public override void calculateDeliveryCost()
+    {
+        Console.WriteLine("Calculating courier delivery cost");
+
+        // Logic here...
+    }
+
+    public override void processDelivery()
+    {
+        Console.WriteLine($"{courierName}'s courier delivering your package");
+
+        // Logic here...
+    }
+}
