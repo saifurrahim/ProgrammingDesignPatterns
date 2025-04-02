9cd3d0b6c95d4d6810c89d3a5053ff886eb25064
unknown <saifurrahimwp@gmail.com>
2025-04-02 22:23:13 +0700

    Make Factory example : add post delivery as concrete class

---
+++ Factory/PostDelivery.cs
@@ -0,0 +1,22 @@
+public class PostDelivery : Delivery {
+    private string postName;
+
+    public CourierDelivery(string postName)
+    {
+        this.postName = postName;
+    }
+
+    public override void calculateDeliveryCost()
+    {
+        Console.WriteLine("Calculating post delivery cost");
+
+        // Logic here...
+    }
+
+    public override void processDelivery()
+    {
+        Console.WriteLine($"{postName}'s post delivering your package");
+
+        // Logic here...
+    }
+}
